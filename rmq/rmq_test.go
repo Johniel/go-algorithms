217b5bc2409fee520a1a5e1968bc661d5ba92624
Johniel <johniel.s.m@gmail.com>
2018-06-27 00:30:31 +0900

    min/max

--- rmq/rmq_test.go
+++ rmq/rmq_test.go
@@ -7,10 +7,11 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func Test(t *testing.T) {
+func TestMin(t *testing.T) {
 	size := 100
 	max := 1 << 30
-	rmq := Init(func(a, b interface{}) bool { return a.(int) < b.(int) }, size, max)
+	cmp := func(a, b interface{}) bool { return a.(int) < b.(int) }
+	rmq := Init(cmp, size, max)
 
 	a := make([]int, size)
 	for i := range a {
@@ -27,10 +28,39 @@ func Test(t *testing.T) {
 		}
 		mn := max
 		for i := begin; i < end; i++ {
-			if a[i] < mn {
+			if cmp(a[i], mn) {
 				mn = a[i]
 			}
 		}
 		assert.Equal(t, rmq.Query(begin, end), mn, "invalid query result")
+	}
+}
+
+func TestMax(t *testing.T) {
+	size := 100
+	min := -1
+	cmp := func(a, b interface{}) bool { return a.(int) > b.(int) }
+	rmq := Init(cmp, size, min)
+
+	a := make([]int, size)
+	for i := range a {
+		x := rand.Intn(10000)
+		a[i] = x
+		rmq.Update(i, x)
+	}
+
+	for try := 0; try < 100; try++ {
+		begin := rand.Intn(size)
+		end := rand.Intn(size)
+		if end < begin {
+			begin, end = end, begin
+		}
+		mx := min
+		for i := begin; i < end; i++ {
+			if cmp(a[i], mx) {
+				mx = a[i]
+			}
+		}
+		assert.Equal(t, rmq.Query(begin, end), mx, "invalid query result")
 	}
 }
